851b7798bb61545074e977a7ca060cd8b68f2fcb
Hydra ☠️ <119828208+Hydra-sjz@users.noreply.github.com>
2023-04-15 17:33:03 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from os import getenv
+from dotenv import load_dotenv
+from os import environ
+
+
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.environ.get("API_ID", ""))
+API_HASH = os.environ.get("API_HASH", "")
